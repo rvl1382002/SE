f3ba0bfd00cbb61f5109735467b58038a43012b6
Riddhish <riddhish.lichade@gmail.com>
2023-01-05 23:41:58 +0530

    otp sharing using oop

--- greeting service.py
+++ greeting service.py
@@ -2,7 +2,20 @@ import smtplib
 import mysql.connector
 import datetime
 
+# birthday la mail
+#
 
+def sendMail(msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)  # connecting to SMTP server at port 587
+        server.ehlo()
+        server.starttls()
+        senderEmail = 'root.rvl@gmail.com'
+        senderPass = 'gdpmznhjhqijiuas'
+        server.login(senderEmail, senderPass)
+    except:
+        print("Unable to connect to the SMTP server")
+        exit()
 
 if __name__=="__main__":
     user="root"
--- web server.py
+++ web server.py
@@ -1,6 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
-import time
 
 hostName = "localhost"
 serverPort = 8080
